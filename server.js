6a5ea3e868c5fbc24f04777f37b08ab2384f434e
efahmed <engineerfa.24816@gmail.com>
2019-03-02 14:57:07 +0000

    first commit

--- server.js
+++ server.js
@@ -1,16 +1,25 @@
 const express = require("express");
-
 const app = express();
-
+const handlebars = require("express-handlebars"); //load the package
+app.engine("handlebars", handlebars());
+app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/views/index.html");
+  // res.render("index.handlebars", {
+  //   firstName: "Fakher",
+  //   lastName: "Ahmed"
+  // });
+  //res.sendFile(__dirname + "/views/index.html");
+  res.render("index", {
+    name: "fakher",
+    date: new Date().toLocaleString()
+  });
 });
 
-app.get("/my-cv", (req, res) => {
-  res.sendFile(__dirname + "/views/my-cv.html");
-});
+// app.get("/my-cv", (req, res) => {
+//   res.render(__dirname + "/views/my-cv.html");
+// });
 
 const SERVER_PORT = process.env.PORT || 3000;
 app.listen(SERVER_PORT, function() {
