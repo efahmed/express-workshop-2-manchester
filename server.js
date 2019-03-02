d146e11d5c31ec1225b73a4586c9e68d959c462e
efahmed <engineerfa.24816@gmail.com>
2019-03-02 15:31:23 +0000

    new commits

--- server.js
+++ server.js
@@ -6,20 +6,19 @@ app.set("view engine", "handlebars");
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  // res.render("index.handlebars", {
-  //   firstName: "Fakher",
-  //   lastName: "Ahmed"
-  // });
   //res.sendFile(__dirname + "/views/index.html");
   res.render("index", {
     name: "fakher",
-    date: new Date().toLocaleString()
+    date: new Date().toLocaleString(),
+    pagename: "index"
   });
 });
 
-// app.get("/my-cv", (req, res) => {
-//   res.render(__dirname + "/views/my-cv.html");
-// });
+app.get("/my-cv", (req, res) => {
+  res.render(__dirname + "/views/my-cv.handlebars", {
+    pagename: "cv"
+  });
+});
 
 const SERVER_PORT = process.env.PORT || 3000;
 app.listen(SERVER_PORT, function() {
